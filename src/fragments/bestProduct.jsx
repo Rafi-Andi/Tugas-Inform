6ff4cd535c8d90a8e4fe094bec0d6bcda29ba1c8
Randi-95 <randiganteng9528@gmail.com>
2025-05-28 22:46:14 +0700

    InstalSwipper

--- src/fragments/bestProduct.jsx
+++ src/fragments/bestProduct.jsx
@@ -1,35 +1,48 @@
+import { Star } from "react-feather";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+
 export const BestProduct = () => {
   return (
     <div className="h-screen bg-white flex items-center px-6">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-12 w-full">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-12 w-full">
         <div>
+           <p className="flex items-center gap-1 bg-primary p-1 py-1.5 text-xs rounded-sm font-semibold w-fit text-white">
+                    <Star size="14" /> Produk Terbaik
+            </p>
           <h1 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4 leading-tight">
-            The best Product for <br />
-            <span>your best time</span>
+           Produk Terbaik Untuk<br />
+            <span className="text-primary">aksesoris anda</span>
           </h1>
           <p className="text-gray-600 mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat et
-            lectus lorem urna, malesuada diam hendrerit.
+           Temukan berbagai aksesoris gaming terbaik untuk menunjang performa dan gaya bermain Anda.
           </p>
-          <button className="bg-indigo-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-indigo-700 transition">
-            Play Now
+          <button className="bg-primary text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-700 transition">
+            Selengkapnya
           </button>
         </div>
 
         <div className="relative">
-          <div className="bg-[#d9d2ff] rounded-3xl p-8 flex justify-center">
-            <img
-              src="/images/watch.png"
-              alt="Smartwatch"
-              className="w-60 md:w-72"
-            />
+          <div className="bg-blue-100 rounded-3xl p-8">
+            <Swiper
+                modules={[Navigation, Pagination]}
+                navigation
+                pagination={{ clickable: true }}
+                spaceBetween={10}
+                className="w-full"
+            >
+              <SwiperSlide>
+                <img src="../../assets/headphoneLogitech.webp" alt="" className="w-60 md:w-72 mx-auto" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="../../assets/HeroFoto.png" alt="" className="w-60 md:w-72 mx-auto" />
+              </SwiperSlide>
+           </Swiper>
           </div>
-          <button className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white rounded-full shadow p-2">
-            <span className="text-xl">&lt;</span>
-          </button>
-          <button className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white rounded-full shadow p-2">
-            <span className="text-xl">&gt;</span>
-          </button>
+          
         </div>
       </div>
         
